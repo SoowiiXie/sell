d10138959d6cefbdaf0a864a84ba7ab929c73355
Soowii <e64009117@gs.ncku.edu.tw>
2020-08-20 18:21:00 +0800

    利用v-once來讓他和大家不會同時改變

--- src/components/app3.vue
+++ src/components/app3.vue
@@ -9,7 +9,7 @@
         
         <div class="input-area mx-auto bg-danger py-2 rounded-lg">
           <button class="button mx-auto rounded-lg bg-success" v-if="!editing" @click="newCard">新增卡片</button>
-          <textarea class="content mx-2 rounded-lg" v-if="editing" v-model="content"></textarea>
+          <textarea class="content mx-2 rounded-lg" v-if="editing" v-model="content" v-once></textarea>
           <button class="button mx-auto rounded-lg" v-if="editing" @click="createCard">建立卡片</button>
           <button class="button mx-auto rounded-lg" v-if="editing" @click="editing=false">取消</button>
         </div>
@@ -28,25 +28,25 @@ import Card from '.././components/card';
 import axios from "axios";
 
 export default {
-  name: "app3",
-  // props: ["item"],  //外面餵進來
-  components: { Card },//引用後註冊元件
-  data: function() {
+  name: "app3",        //js裡的el 掛載元素
+  // props: ["item"],  //props 是從外面餵進來的資料
+  components: { Card },//components 是引用後需註冊的元件
+  data: function() {   //data 資料
     return {
       keyword: "",
       list777: [],
       content: "",
       editing: false
     };
-  },
-  computed: {
+  },                   //watch 用於監聽資料是否有變動
+  computed: {          //computed 用於要改變已經存在的資料
     filteredList: function() {
       return this.list777.filter(item => {
         return item.o_tlc_agency_name.indexOf(this.keyword) !== -1;
       });
     }
   },
-  methods: {
+  methods: {           //methods 方法
     newCard(event){
       event.preventDefault();
       this.editing=true;
@@ -57,10 +57,11 @@ export default {
     createCard(event){
       event.preventDefault();
       this.list777.splice(0, 0,JSON.parse("{\"o_tlc_agency_name\" : \""+this.content+"\",\"o_tlc_agency_address\" : \""+this.content+"\"}"));
+      this.content= "";
       this.editing=false;
     }
   },
-  mounted: function() {
+  mounted: function() {//mounted：元素已掛載， $el 被建立。
     axios
       .get(
         "https://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=24c9f8fe-88db-4a6e-895c-498fbc94df94"
